56b6d4fc46f2ed78122cc904b0427590cc957a01
Jorge Magallanes <jmagalla@espol.edu.ec>
2024-05-02 13:09:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-### Autor: Escriba aquí su nombre
-### Fecha: Escriba la fecha
-### 
+### Llenar los siguientes comentarios: 
+### Apellidos: 
+### Nombres: 
+
+### A partir de aquí escriba el código para el ejercicio 1
+print('Hola Mundo!')
+
+### A partir de aquí escriba el código para el ejercicio 2
+print('Hola clase de FP')
 
